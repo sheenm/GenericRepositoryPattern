d4ed78e1300a61913481e807d1ba2ee98affbece
SheenM <sheenm@ya.ru>
2018-05-13 03:47:48 +1000

    Realization for save method. not sure it works, need to test it.

--- Repositories/AdoRepository.cs
+++ Repositories/AdoRepository.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Data;
 using GenericRepositoryPattern.Database;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 
 namespace GenericRepositoryPattern.Repositories
 {
@@ -10,11 +12,13 @@ namespace GenericRepositoryPattern.Repositories
     {
         IDbConnectionProvider _dbProvider;
         private string _tableName;
+        private PropertyInfo[] _objectProperties;
 
         public AdoRepository(IDbConnectionProvider dbProvider, string tableName)
         {
             _dbProvider = dbProvider;
             _tableName = tableName;
+            _objectProperties = typeof(T).GetProperties();
         }
 
         public T GetById(int id)
@@ -28,7 +32,7 @@ namespace GenericRepositoryPattern.Repositories
                 parameter.Direction = ParameterDirection.Input;
                 parameter.DbType = DbType.Int32;
 
-                command.CommandText = $"SELECT * FROM {_tableName} WHERE id=@ID";
+                command.CommandText = $"SELECT * FROM {_tableName} WHERE Id=@ID";
                 command.CommandType = CommandType.Text;
                 command.Parameters.Add(parameter);
 
@@ -68,8 +72,77 @@ namespace GenericRepositoryPattern.Repositories
 
         public bool Save(T item)
         {
-            throw new NotImplementedException();
+            using (var connection = _dbProvider.GetDatabaseConnection())
+            using (var command = connection.CreateCommand())
+            {
+                foreach (var property in _objectProperties)
+                {
+                    var parameter = command.CreateParameter();
+                    parameter.ParameterName = GetParameterName(property);
+                    parameter.Value = property.GetValue(item);
+                    parameter.Direction = ParameterDirection.Input;
+                    parameter.DbType = GetDbType(property.GetType());
+                    command.Parameters.Add(parameter);
+                }
+
+                command.CommandText = GetCommandTextForSave(item);
+                command.CommandType = CommandType.Text;
+
+                return command.ExecuteNonQuery() == 1;
+            }
         }
+
+        private string GetCommandTextForSave(T item)
+        {
+            var commandTextBuilder = new StringBuilder($"UPDATE {_tableName} SET");
+            foreach (var property in _objectProperties)
+            {
+                commandTextBuilder.Append($" {property.Name}={GetParameterName(property)}, ");
+            }
+            commandTextBuilder.Remove(commandTextBuilder.Length - 1, 1);
+            commandTextBuilder.Append($" WHERE  Id={GetId(item)}");
+
+            return commandTextBuilder.ToString();
+        }
+
+        private static string GetParameterName(PropertyInfo propertyInfo)
+        {
+            return $"@{propertyInfo.Name}";
+        }
+
+        private DbType GetDbType(Type type)
+        {
+            if (type == typeof(int))
+            {
+                return DbType.Int32;
+            }
+            if (type == typeof(string))
+            {
+                return DbType.String;
+            }
+            if (type == typeof(DateTime))
+            {
+                return DbType.DateTime;
+            }
+            if (type == typeof(byte[]))
+            {
+                return DbType.Binary;
+            }
+
+            throw new NotImplementedException($"There's no converter for type {type.Name} in AdoRepository");
+        }
+
+        private int GetId(T item)
+        {
+            foreach (var property in _objectProperties)
+            {
+                if (property.Name == "Id")
+                    return (int)property.GetValue(item);
+            }
+
+            throw new ArgumentException($"Id property was not found in {typeof(T).Name} ");
+        }
+
 
         protected abstract T PopulateRecord(IDataReader reader);
     }
